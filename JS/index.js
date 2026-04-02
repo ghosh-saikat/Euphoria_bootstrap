a28a1a3c9595e6e1157434258842a707e8838c4c
ghosh-saikat <2001saikatghosh@gmail.com>
2026-04-02 22:26:09 +0530

    third commit

--- JS/index.js
+++ JS/index.js
@@ -7,15 +7,14 @@ $("document").ready(() => {
 /*Gallery-section */
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  cssMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
